2369907ee1e443e79356c06d0dc0fd878a371f8f
Khimki-pro <vadimshoh@gmail.com>
2020-03-22 19:05:08 +0300

    Подключил фильтры к сообщениям

--- src/main/java/org/example/shenanigans/GreetingController.java
+++ src/main/java/org/example/shenanigans/GreetingController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -35,5 +36,21 @@ public class GreetingController {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
         return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model){
+        Iterable<Message> messages;
+
+        if(filter != null && !filter.isEmpty()){
+            messages = messageRepo.findByTag(filter);
+        }
+        else{
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
+
+        return "main";
+
     }
 }
--- src/main/java/org/example/shenanigans/repos/MessageRepo.java
+++ src/main/java/org/example/shenanigans/repos/MessageRepo.java
@@ -3,5 +3,8 @@ package org.example.shenanigans.repos;
 import org.example.shenanigans.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
 }
